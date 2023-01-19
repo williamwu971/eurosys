addc03100b55ad7bf67e7d9b41a7227e4aeca03d
William <xiwu9085@gmail.com>
2023-01-19 17:17:55 +1100

    -AUTO- Thu Jan 19 05:17:55 PM AEDT 2023

--- run.bash
+++ run.bash
@@ -9,7 +9,7 @@ if [ "$#" -ne 1 ]; then
   #  taskset -c 12 ./kernel.out 142800 320 || exit
   #  taskset -c 12 ./kv.out 1000000 1 || exit
   rm -rf /pmem0/* && taskset -c 12 ./fs_expand.out 25600 "$((4 * 1024 * 1024))" || exit
-  rm -rf /pmem0/* && taskset -c 12 ./fs_open.out 25600 "$((4 * 1024 * 1024))" || exit
+#  rm -rf /pmem0/* && taskset -c 12 ./fs_open.out 25600 "$((4 * 1024 * 1024))" || exit
 
   cd ..
 else
