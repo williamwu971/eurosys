12968c1c7d9a14f14309c255b56c62a8762586b7
William <xiwu9085@gmail.com>
2023-01-19 18:17:39 +1100

    -AUTO- Thu Jan 19 06:17:39 PM AEDT 2023

--- run.bash
+++ run.bash
@@ -5,11 +5,11 @@ if [ "$#" -ne 1 ]; then
   cd build/ || exit
   rm -rf ./*.txt
 
-  #  taskset -c 12 ./ralloc.out 142800 320 || exit
-  #  taskset -c 12 ./kernel.out 142800 320 || exit
-  #  taskset -c 12 ./kv.out 1000000 1 || exit
-  rm -rf /pmem0/* && taskset -c 12 ./fs_expand.out 2560 "$((4 * 1024 * 1024))" || exit
-#  rm -rf /pmem0/* && taskset -c 12 ./fs_open.out 2560 "$((4 * 1024 * 1024))" || exit
+  taskset -c 12 ./ralloc.out 142800 320 || exit
+  taskset -c 12 ./kernel.out 142800 320 || exit
+  taskset -c 12 ./kv.out 1000000 1 || exit
+  #  rm -rf /pmem0/* && taskset -c 12 ./fs_expand.out 2560 "$((4 * 1024 * 1024))" || exit
+  #  rm -rf /pmem0/* && taskset -c 12 ./fs_open.out 2560 "$((4 * 1024 * 1024))" || exit
 
   cd ..
 else
