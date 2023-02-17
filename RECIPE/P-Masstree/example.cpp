c6f6f1ac2f29ff9f5f1f719b3594218ccf8aea89
William <xiwu9085@gmail.com>
2023-02-17 16:24:17 +1100

    -AUTO- Fri Feb 17 04:24:17 PM AEDT 2023

--- RECIPE/P-Masstree/example.cpp
+++ RECIPE/P-Masstree/example.cpp
@@ -187,7 +187,9 @@ void run(char **argv) {
 //                }
 
                 buffer[0] = keys[i];
-                pmem_memcpy_persist(value, buffer, size);
+//                pmem_memcpy_persist(value, buffer, size);
+                memcpy(value, buffer, size);
+                pmem_persist(value, size);
 
 
                 uint64_t pt0 = readTSC(1, 1);
