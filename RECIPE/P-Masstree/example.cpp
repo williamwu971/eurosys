062990fe4985989cf0f88900c10b87fb4a400496
William <xiwu9085@gmail.com>
2023-02-28 17:50:15 +1100

    -AUTO- Tue Feb 28 05:50:15 PM AEDT 2023

--- RECIPE/P-Masstree/example.cpp
+++ RECIPE/P-Masstree/example.cpp
@@ -70,6 +70,11 @@ int bench_start() {
             getpid());
     res &= system(command);
 
+    sprintf(command, "sudo /home/blepers/linux-huge/tools/perf/perf mem record "
+                     "-p >> perf_mem.out 2>&1 &"
+    );
+    res &= system(command);
+
     res &= system("/mnt/sdb/xiaoxiang/pcm/build/bin/pcm-memory -all >pcm-memory.log 2>&1 &");
     sleep(1);
 
